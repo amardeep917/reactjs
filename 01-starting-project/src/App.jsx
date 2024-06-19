c5cfa8d4aa3f071bfdf1dda296654c3b064f40e0
amardeep <amardeep@lyrostech.com>
2024-06-19 18:23:32 +0530

    starting project

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    return(
+      <div>
+        <h1>project starting</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam autem consequatur nemo, mollitia minima fugit assumenda est voluptas quos impedit? </p>
+      </div>
+      
+    );
+}
+
+export default App
